e674701b793156ea05013dc57bad9fcb1d775093
Jimmy Hutson <jimmy@hutsonandfriends.com>
2022-04-14 19:07:45 -0700

    Define board line marking behavior

--- src/dotsquares/board.py
+++ src/dotsquares/board.py
@@ -1,48 +1,70 @@
 
-# odds - H
-# evens - V
+# horizontal line - H
+# vertical line - V
 
-# 3 column, 2 rows
+# ex. 2 rows, 3 columns
 #   H H H
-#  V V V V
+#  V-V-V-V
 #   H H H 
-#  V V V V
+#  V-V-V-V
 #   H H H
-
-# m columns x n rows
-# m Hs per column for a row
-# m + 1 Vs per column for a row
-# n V rows
-# n + 1 H rows
-# m(n+1) + n(m+1) total H and V items
-# f(m,n) = m(n+1) + n(m+1)
-
-# m = 3, n = 2
-# 3(2+1) + 2(3+1) 
-# 3(3) + 2(4)
-# 9 + 8
-# 17 yes
-
-# 30x30, m = n = 30
-# f(m,n) = m(n+1) + n(m+1)
-# f(30,30) = 30(30+1) + 30(30+1)
-#  = 2(30(31))
-#  = 1860
-
-# total storage for lines (above) and boxes:
-# t(m,n) = f(m,n) + mn
-#        = m(n+1) + n(m+1) + mn
-#        = mn + m + mn + n + mn
-#        = 3mn + m + n
-# t(30,30) = 3(30)(30) + 30 + 30
-# t(30,30) = 3(30)(30) + 2(30)
-# t(30,30) = 30(3(30) + 2)
-# t(30,30) = 30(92)
-# t(30,30) = 2760
 
 import numpy as np
 
 class Board:
-    def __init__(self, columns, rows):
-        self.lines = np.zeros((columns + 1, rows + 1), dtype=np.bool_)
-        self.boxes = np.zeros((columns, rows), dtype=np.bool_)
+    def __init__(self, rows, columns):
+        self.lines = np.zeros((2 * rows + 1, columns + 1), dtype=np.byte)
+        self.boxes = np.zeros((rows, columns), dtype=np.byte)
+
+
+    def markLine(self, row: int, column: int, player: int):
+
+        if row % 2 == 0 and column >= self.lines.shape[1]:
+            raise IndexError
+
+        if self.lines[row, column] == 0:
+            self.lines[row, column] = player
+            boxes = self.checkForFilledBoxes(row, column, player)
+
+            for box in boxes:
+                self.boxes[box] = player
+
+            return boxes
+
+        return None
+
+
+    def checkForFilledBoxes(self, lineRow: int, lineColumn: int, player: int):
+        filled = []
+
+        if lineRow % 2 == 0: # horizontal line
+            # check square above
+            if lineRow > 0:
+                if self.lines[(lineRow - 2, lineColumn)] > 0:
+                    if self.lines[(lineRow - 1, lineColumn)] > 0 and self.lines[(lineRow - 1, lineColumn + 1)] > 0:
+                        box = (lineRow // 2 - 1, lineColumn)
+                        filled.append(box)
+
+            # check square below
+            if lineRow + 1 < self.lines.shape[0]:
+                if self.lines[(lineRow + 2, lineColumn)] > 0:
+                    if self.lines[(lineRow + 1, lineColumn)] > 0 and self.lines[(lineRow + 1, lineColumn + 1)] > 0:
+                        box = (lineRow // 2, lineColumn)
+                        filled.append(box)
+
+        else: # vertical line
+            # check square to left
+            if lineColumn > 0:
+                if self.lines[(lineRow, lineColumn - 1)] > 0:
+                    if self.lines[(lineRow - 1, lineColumn - 1)] > 0 and self.lines[(lineRow + 1, lineColumn - 1)] > 0:
+                        box = ((lineRow - 1) // 2, lineColumn - 1)
+                        filled.append(box)
+
+            # check square to right
+            if lineColumn + 1 < self.lines.shape[1]:
+                if self.lines[(lineRow, lineColumn + 1)] > 0:
+                    if self.lines[(lineRow - 1, lineColumn)] > 0 and self.lines[(lineRow + 1, lineColumn)] > 0:
+                        box = ((lineRow - 1) // 2, lineColumn)
+                        filled.append(box)
+
+        return filled
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,51 @@
+import pytest
 from dotsquares import Board
 
-def test_first():
-    board = Board(3,2)
-    assert board.boxes.shape == (3,2)
+@pytest.mark.parametrize("boxShape,expectedLineShape", [
+    ((1, 1), (3, 2)),
+    ((2, 3), (5, 4))
+])
+def test_one_square_line_shape(boxShape, expectedLineShape):
+    board = Board(*boxShape)
+    assert(board.lines.shape == expectedLineShape)
+
+
+def test_line_marked_once():
+    board = Board(1, 1)
+    board.markLine(0, 0, 1)
+    assert(board.markLine(0, 0, 1) == None)
+
+
+@pytest.mark.parametrize("index", [0, 1, 2, 3])
+def test_make_square(index):
+    lines = [(0, 0), (1, 1), (2, 0), (1, 0)]
+    lines = lines[index:] + lines[:index]
+
+    board = Board(1, 1)
+
+    assert(board.markLine(*lines[0], 1) == [])
+    assert(board.markLine(*lines[1], 2) == [])
+    assert(board.markLine(*lines[2], 1) == [])
+    assert(board.markLine(*lines[3], 2) == [(0,0)])
+
+
+def test_make_two_squares_from_vertical_line():
+    board = Board(1, 2)
+
+    player = 1
+    for line in [(0, 0), (0, 1), (1, 0), (1, 2), (2, 0), (2, 1)]:
+        assert(board.markLine(*line, player) == [])
+        player = player % 2 + 1
+
+    assert(board.markLine(1, 1, player) == [(0, 0), (0, 1)])
+
+
+def test_make_two_squares_from_horizontal_line():
+    board = Board(2, 1)
+
+    player = 1
+    for line in [(0, 0), (1, 0), (1, 1), (3, 0), (3, 1), (4, 0)]:
+        assert(board.markLine(*line, player) == [])
+        player = player % 2 + 1
+
+    assert(board.markLine(2, 0, player) == [(0, 0), (1, 0)])
