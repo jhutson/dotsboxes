5b85fad674f00ef05b116d8eafffbfc898b88e3e
Jimmy Hutson <jimmy@hutsonandfriends.com>
2023-01-17 13:48:48 -0800

    Add nginx to proxy frontend locally

--- frontend/game/local.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    define: {
-        GLOBAL_API_BASE_URL: '"http://localhost:8080/api/v1/game"',
-        GLOBAL_EVENTS_BASE_URL: '"ws://localhost:8080/events/v1/game"',
-        GLOBAL_FETCH_MODE: '"cors"',
-        GLOBAL_FETCH_CREDENTIALS: '"omit"'
-    }
-}
---
+++ frontend/game/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command, mode }) => {
+    if (command === 'serve') {
+        return {
+            define: {
+                GLOBAL_API_BASE_URL: '"http://localhost:8080/api/v1/game"',
+                GLOBAL_EVENTS_BASE_URL: '"ws://localhost:8080/events/v1/game"',
+                GLOBAL_FETCH_MODE: '"cors"',
+                GLOBAL_FETCH_CREDENTIALS: '"omit"'
+            },
+            server: {
+                host: true
+            }
+        }
+    } else { // build
+        return {
+            define: {
+                GLOBAL_API_BASE_URL: '"http://localhost:8080/api/v1/game"',
+                GLOBAL_EVENTS_BASE_URL: '"ws://localhost:8080/events/v1/game"',
+                GLOBAL_FETCH_MODE: '"same-origin"',
+                GLOBAL_FETCH_CREDENTIALS: '"same-origin"'
+            }
+        }
+    }
+})
