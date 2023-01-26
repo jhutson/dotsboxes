dd92e9a86c328c82242d2f9acc4bfcf883ad1d01
Jimmy Hutson <jimmy@hutsonandfriends.com>
2023-01-25 17:16:31 -0800

    Add lobby service boilerplate

---
+++ lobby-service/src/main/java/com/hutsondev/lobbyservice/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.hutsondev.lobbyservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+  // TODO: Re-enable security authorization
+  // TODO: Re-enable CSRF
+  @Bean
+  public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+    return http.authorizeExchange(authorize ->
+            authorize.anyExchange().permitAll()
+        )
+        .csrf(CsrfSpec::disable)
+        .build();
+  }
+}
